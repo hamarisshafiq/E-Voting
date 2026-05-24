cb7b0339fc3ad9000140d68371a2949f005a860f
Hamaris Shafiq <hamarisshafiq@gmail.com>
2026-05-24 14:13:50 +0500

    Delete admin directory

--- admin/end_election.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "../db.php";
-
-$conn->query("UPDATE settings SET is_election_active=0 WHERE id=1");
-
-header("Location: dashboard.php");
-?>
--- admin/start_election.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "../db.php";
-
-$conn->query("UPDATE settings SET is_election_active=1 WHERE id=1");
-
-header("Location: dashboard.php");
-?>
